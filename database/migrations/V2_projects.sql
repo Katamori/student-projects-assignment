a29402824db0169dc3b8c70b9c8553f8567287e7
Katamori <zoltanschmidt@protonmail.com>
2020-06-26 01:22:49 +0200

    define management microservice
    + small fixes

--- database/migrations/V2_projects.sql
+++ database/migrations/V2_projects.sql
@@ -1,6 +1,6 @@
 -- CREATE TABLE "projects" -------------------------------------
 CREATE TABLE IF NOT EXISTS `projects`( 
-	`id` Int( 11 ) UNSIGNED AUTO_INCREMENT NOT NULL,
+	`id` Int( 11 ) AUTO_INCREMENT NOT NULL,
 	`name` VarChar( 128 ) NOT NULL,
 	`description` VarChar( 255 ) NOT NULL,
 	`createdAt` DateTime NULL,
